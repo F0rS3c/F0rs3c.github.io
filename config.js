839c08cdead7a418cbc3ab1a4ea3909586e1e8c6
F0rS3c <58636788+F0rS3c@users.noreply.github.com>
2025-12-07 06:03:08 +0100

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// Copy this file to config.js and add your actual API key
-// This file is safe to commit - it's just a template
-
-window.CONFIG = {
-    WEB3FORMS_API_KEY: 'f62c67aa-cbe7-4bbd-a81c-d47ef95f67b4'
-};
-
